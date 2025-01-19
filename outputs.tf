b977597fa5872d73d16ea3af5fe71ae89d81312b
teliVighnesh04 <vighneshteli14@gmail.com>
2025-01-19 23:08:15 +0530

    output block  added

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "web_ip22" {
   value       = aws_instance.web22.public_ip
   description = "Public IP of Web instance 22"
 }
+
+output "environment" {
+  value       = var.environment
+  description = "Environment name"
+}

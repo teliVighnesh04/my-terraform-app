37df71ed46dee85f1b6e16a479992bf5dd42a581
teliVighnesh04 <vighneshteli14@gmail.com>
2025-01-19 23:02:23 +0530

    new instance added

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "web_ip" {
+  value       = aws_instance.web.public_ip
+  description = "Public IP of Web instance"
+}
+
+output "web_ip22" {
+  value       = aws_instance.web22.public_ip
+  description = "Public IP of Web instance 22"
+}
